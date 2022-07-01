b0eb86390b86bc0c86d39dc1cd1d33169cb2f423
chinki kumari singh <singhchinkikumari.com>
2022-07-01 22:53:09 +0530

    [Chinki] Update vaule in json file

--- EmployeeAPI/RestAPITest/RestSharpTest.cs
+++ EmployeeAPI/RestAPITest/RestSharpTest.cs
@@ -55,5 +55,19 @@ namespace RestAPITest
                 Console.WriteLine(response.Content);
             });
         }
+        [Test]
+        public void OnUpdateRequest_ShouldUpdateInJSONServer()
+        {
+            restClient = new RestClient("http://localhost:4000");
+            RestRequest request = new RestRequest("/employees/3", Method.Put);
+            var body = new Employee { Id = 23, Name = "kamini", Salary = "29800" };
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            RestResponse response = restClient.Execute(request);
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.OK);
+            Employee data = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual(body.Name, data.Name);
+            Assert.AreEqual(body.Salary, data.Salary);
+            Console.WriteLine(response.Content);
+        }
     }
 }
