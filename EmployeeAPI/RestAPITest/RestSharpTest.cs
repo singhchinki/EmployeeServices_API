ad8ffa69b85f80b8c8ce9a3537ed5b3aac518c3f
chinki kumari singh <singhchinkikumari.com>
2022-07-01 22:32:53 +0530

    [Chinki] Add data in json

--- EmployeeAPI/RestAPITest/RestSharpTest.cs
+++ EmployeeAPI/RestAPITest/RestSharpTest.cs
@@ -22,5 +22,19 @@ namespace RestAPITest
                 Console.WriteLine("ID: " + e.Id + "\nName: " + e.Name + "\nSalary: " + e.Salary);
             }
         }
+        [Test]
+        public void OnPostingEmployeeData_ShouldAddtoJsonServer()
+        {
+            restClient = new RestClient("http://localhost:4000");
+            RestRequest request = new RestRequest("/employees", Method.Post);
+            var body = new Employee { Id = 25, Name = "kavita", Salary = "29000" };
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            RestResponse response = restClient.Execute(request);
+            Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
+            Employee data = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual("Kavita", data.Name);
+            Assert.AreEqual("29000", data.Salary);
+            Console.WriteLine(response.Content);
+        }
     }
 }
